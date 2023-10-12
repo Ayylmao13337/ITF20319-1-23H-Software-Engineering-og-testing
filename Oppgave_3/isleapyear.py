e691bf1426cde078d73db8d7d6fd42ba5e102bf2
Trym <hamretrym@gmail.com>
2023-10-12 16:24:32 +0200

    ja

---
+++ Oppgave_3/isleapyear.py
@@ -0,0 +1,15 @@
+
+
+def isLeapYear(year):
+    if year%100 != 0 and year%4 == 0:
+        return True
+    elif year%400 == 0:
+        return True
+    else:
+        return False
+
+    
+
+
+#print(isLeapYear(2004))
+
